0bbef1814535e726b9074d51429539d269335902
[stefanhallberg123] <[stefanhalllberg@live.se]>
2020-05-22 19:58:49 +0200

    fixing some images

--- config/config.js
+++ config/config.js
@@ -1,9 +1,8 @@
-if (process.env.NODE_ENV !== "production")
-    require("dotenv").config()
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
 const config = {
-    databaseURL: process.env.databaseURL,
-    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-    STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY
+  databaseURL: process.env.databaseURL,
+  STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+  STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
 };
 module.exports = config;
